d95325c56a63b2dd85393f09b0204789f06acc91
jmdeluca90 <jmdeluca90@gmail.com>
2025-10-02 19:32:28 -0400

    add longest substring

---
+++ LeetCode/LongestSubstring.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice.jd.LeetCode
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int l = s.Length;
+            if (l == 0) return 0;
+            var dict = new Dictionary<char, int>();
+            int maxLength = 0, start = 0;
+
+            for (int end = 0; end < l; end++)
+            {
+                char c = s[end];
+                if (dict.ContainsKey(c))
+                {
+                    start = Math.Max(dict[c] + 1, start);
+                }
+                dict[c] = end;
+                maxLength = Math.Max(maxLength, end - start + 1);
+            }
+            return maxLength;
+        }
+    }
+}
