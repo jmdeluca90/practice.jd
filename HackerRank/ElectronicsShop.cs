71f576872a5b173d56469b32b001b717fd264056
jmdeluca90 <jmdeluca90@gmail.com>
2025-10-15 20:26:04 -0400

    beep boop ba doop

---
+++ HackerRank/ElectronicsShop.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice.jd.HackerRank
+{
+    public class ElectronicsShop
+    {
+        public static int getMoneySpent(int[] keyboards, int[] drives, int b)
+        {
+            int maxCost = -1;
+            // return -1 if budget does not cover can be simplified by just setting maxCost to -1 initially
+
+            // also could just do a foreach check here
+            // for (int i = 0; i < keyboards.Length; i++)
+            foreach (int i in keyboards)
+            {
+                // for (int j = 0; j < drives.Length; j++)
+                foreach (int j in drives)
+                {
+                    // int sum = keyboards[i] + drives[j];
+                    int sum = i + j;
+                    maxCost = sum > maxCost && sum <= b ? sum : maxCost;
+                }
+            }
+            return maxCost;
+        }
+        public static int getMoneySpentUsingSortAndTwoPointer(int[] keyboards, int[] drives, int b)
+        {
+            Array.Sort(keyboards); // sort both ascending
+            Array.Sort(drives);
+
+            int maxCost = -1;
+            int i = 0; // keyboard pointer (low to high)
+            int j = drives.Length - 1; // drive pointer (high to low)
+
+            while (i < keyboards.Length && j >= 0)
+            {
+                int sum = keyboards[i] + drives[j];
+
+                if (sum > b)
+                {
+                    j--; // too expensive, need to move to a cheaper drive
+                }
+                else
+                {
+                    if (sum > maxCost)
+                        maxCost = sum;
+
+                    i++; // try a more expensive keyboard
+                }
+            }
+            return maxCost;
+        }
+    }
+}
---
+++ HackerRank/SockMatching.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice.jd.HackerRank
+{
+    public class SockMatching
+    {
+        // this one was easy but could probably be optimized - solution: O(n) time and O(k) space where k = # of unique colored socks
+        public static int SockMerchant(int n, List<int> ar)
+        {
+            int pairs = 0;
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (int sock in ar)
+            {
+                dict.TryAdd(sock, 0);
+                dict[sock]++;
+            }
+
+            foreach (int color in dict.Keys)
+            {
+                pairs += dict[color] / 2;
+            }
+            return pairs;
+        }
+        // yep this can definitely be optimized to only loop throughthe dictionary once - solution: still O(n) time and O(k) space, but only loop once through dictionary
+        public static int SockMerchantOptimized(int n, List<int> ar)
+        {
+            int pairs = 0;
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            foreach (int sock in ar)
+            {
+                if (dict.ContainsKey(sock))
+                {
+                    dict[sock]++;
+                    if (dict[sock] % 2 == 0)
+                        pairs++;
+                }
+                else
+                {
+                    dict[sock] = 1;
+                }
+            }
+            return pairs;
+        }
+        // but what if we could optimize even more? constraints tell us that n (# of socks) will be between 1-100 and ar[i] (colors of sock) will be between 0-99
+        // with that in mind, we can use an array to solve the problem with O(n) time and O(1) space
+        public static int SockMerchantOptimizedWithArray(int n, List<int> ar)
+        {
+            int pairs = 0;
+            int[] arr = new int[101];
+
+            foreach (int sock in ar)
+            {
+                    arr[sock]++;
+                    if (arr[sock] % 2 == 0)
+                        pairs++;
+            }
+            return pairs;
+        }
+    }
+}
