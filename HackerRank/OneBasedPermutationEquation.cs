1764a7051d52f25a1dd7f49b480d7e9e79b556ae
jmdeluca90 <jmdeluca90@gmail.com>
2025-10-19 16:43:35 -0500

    hmm

---
+++ HackerRank/OneBasedPermutationEquation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice.jd.HackerRank
+{
+    internal class OneBasedPermutationEquation
+    {
+        public static List<int> permutationEquation(List<int> p)
+        {
+            List<int> result = [];
+            int n = p.Count;
+            for (int i = 1; i <= n; i++)
+            {
+                int p2 = p.IndexOf(i);
+                int p3 = p.IndexOf(p2 + 1);
+                result.Add(p3 + 1);
+            }
+            return result;
+        }
+    }
+}
