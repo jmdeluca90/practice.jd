67380cd30ce5e680d0f9a9ad565186d8af953d35
jmdeluca90 <jmdeluca90@gmail.com>
2025-10-19 20:26:53 -0500

    this one was fun but hackerrank is actually kinda dumb and they gave certain constraints but didn't even follow them in their test cases

---
+++ HackerRank/JayingOnCs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice.jd.HackerRank
+{
+    public class JayingOnCs
+    {
+        public static int jumpingOnClouds(int[] c, int k)
+        {
+            int e = 100; 
+            int n = c.Length;
+            int currentIndex = 0;
+
+            while (e > 0)
+            {
+                currentIndex = (currentIndex + k) % n; // set the index to current value + value of the jump % n
+                e--; // base energy being used
+
+                if (c[currentIndex] == 1) // if we land on thundercloud
+                    e -= 2;
+                if (currentIndex == 0) // if we loop back to zero we return e energy
+                    return e;
+            }
+            return e;
+
+        }
+    }
+}
