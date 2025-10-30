07f1f667443e31cf63d44b0617aba87f8ed6ddb7
jmdeluca90 <jmdeluca90@gmail.com>
2025-10-29 20:14:19 -0500

    pushing more solutions

---
+++ HackerRank/APCICPCTeam.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice.jd.HackerRank
+{
+    public class APCICPCTeam
+    {
+        public static List<int> acmTeam(List<string> topic)
+        {
+            int n = topic.Count;
+            int max = 0;
+            int teamMax = 0;
+            // returns a list of int - max topics and # of teams that know that many topics
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    // int a = Convert.ToInt32(topic[i], 2);
+                    // int b = Convert.ToInt32(topic[j], 2);
+                    // int combined = a | b;
+                    // int current = CountBits(combined);
+                    int current = 0;
+                    for (int k = 0; k < topic[i].Length; k++)
+                    {
+                        if (topic[i][k] == '1' || topic[j][k] == '1')
+                            current++;
+                    }
+                    if (current > max)
+                    {
+                        max = current;
+                        teamMax = 1;
+                    }
+                    else if (current == max)
+                    {
+                        teamMax++;
+                    }
+                }
+            }
+            return new List<int> { max, teamMax };
+        }
+        public static int CountBits(int n)
+        {
+            int count = 0;
+            while (n > 0)
+            {
+                if ((n & 1) == 1)
+                    count++;
+                n >>= 1;
+            }
+            return count;
+        }
+    }
+}
---
+++ HackerRank/TimeInWords.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice.jd.HackerRank
+{
+    public class TimeInWords
+    {
+        public static string ToimeInWords(int h, int m)
+        {
+            // set up dictionary with values to map int to correct string phrasing for problem
+            var dict = new Dictionary<int, string>()
+            {
+                [1] = "one",
+                [2] = "two",
+                [3] = "three",
+                [4] = "four",
+                [5] = "five",
+                [6] = "six",
+                [7] = "seven",
+                [8] = "eight",
+                [9] = "nine",
+                [10] = "ten",
+                [11] = "eleven",
+                [12] = "twelve",
+                [13] = "thirteen",
+                [14] = "fourteen",
+                [15] = "quarter",
+                [16] = "sixteen",
+                [17] = "seventeen",
+                [18] = "eighteen",
+                [19] = "nineteen",
+                [20] = "twenty",
+                [30] = "half"
+            };
+            for (int i = 21; i < 30; i++)
+                dict[i] = "twenty " + dict[i - 20];
+
+            if (m == 0)
+                return $"{dict[h]} o' clock"; // "hour" o' clock
+            else if (m == 15 || m == 30)
+                return $"{dict[m]} past {dict[h]}";
+            else if (m == 45)
+                return $"quarter to {dict[h + 1]}";
+            else if (m <= 30)
+                return $"{dict[m]} minute{(m == 1 ? "" : "s")} past {dict[h]}"; // __ minutes "past" hour
+            else
+                return $"{dict[60 - m]} minute{(60 - m == 1 ? "" : "s")} to {dict[h + 1]}"; // ___ minutes "to" hour       
+        }
+    }
+}
